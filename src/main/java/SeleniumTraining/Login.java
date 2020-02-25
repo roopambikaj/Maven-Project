04d4166f02dd2ce5665aafd14889e72a42ee5521
Roopambika <roopambikaj@gmail.com>
2020-02-25 11:22:09 +0000

    Maven_project commits

---
+++ src/main/java/SeleniumTraining/Login.java
@@ -0,0 +1,18 @@
+package SeleniumTraining;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	@Test
+	public void loginTestPOM() {
+	
+//	Initialize driver
+//		WebDriver driver = utilities.DriverFactory.open("firefox");
+		
+//	Enter login details
+//	Confirmation
+//	Close the driver
+	}
+}
